60d1ab882a9014694c591efbe844612f7d2ab8d1
shikdernyc <shikdernyc@gmail.com>
2018-07-25 16:22:20 +0000

    Added states for navItems and rendering Navbar inside app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      navItems: [
+        {name: 'Home', href: "#home"},
+        {name: 'About', href: '#about'},
+        {name: 'Skills', href: '#skills'},
+        {name: 'Projects', href: '#projects'},
+        {name: 'Contact', href: '#contact'}
+      ]
+    }
+  }
+  
   render() {
     return (
       <div>
-        
+        <Navbar navItems={this.state.navItems}/>
       </div>
     );
   }
