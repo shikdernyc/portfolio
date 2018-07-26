b20124b1f41066e7c133ec819a757cbf9f3f8886
shikdernyc <shikdernyc@gmail.com>
2018-07-26 02:29:09 +0000

    Updated Stylistic changes

--- src/Home.js
+++ src/Home.js
@@ -6,16 +6,15 @@ class Home extends Component{
         super(props)
         this.title = "Asif Shikder"
         this.subTitle = "Student Programmer"
-        // this.coverUrl = props.coverUrl
-        // this.avatarUrl = props.avatarUrl
+        this.avatarUrl = "https://s3.us-east-2.amazonaws.com/shikdernyc-portfolio/avatar.jpg"
     }
     
     render(){
         return (
-          <header className="masthead">
+          <header id="home" className="masthead">
             <div className="container d-flex h-100 align-items-center">
               <div className="mx-auto text-center">
-                <img id="avatar" src='https://s3.us-east-2.amazonaws.com/shikdernyc-portfolio/avatar.jpg' alt='avatar'/>
+                <img id="avatar" src={this.avatarUrl} alt='avatar'/>
                 <h1 className="text-white mx-auto mt-2">{this.title}</h1>
                 <h2 className="text-white mx-auto mt-1 mb-2">{this.subTitle}</h2>
               </div>
