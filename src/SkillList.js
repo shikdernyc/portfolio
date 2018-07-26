44d88b8062b4cd7a0b07b194155d452d2fb3b752
shikdernyc <shikdernyc@gmail.com>
2018-07-26 20:25:23 +0000

    Added a description field

--- src/SkillList.js
+++ src/SkillList.js
@@ -1,34 +1,42 @@
 const skills = [
     {
         title: "Java",
+        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-java"
     },
     {
         title: "Python",
+        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-python"
     },
     {
         title: "Javascript",
+        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-js"
     },
     {
         title: "HTML & CSS",
+        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-html5"
     },
     {
         title: "NodeJS",
+        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-node-js"
     },
     {
         title: "React",
+        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-react"
     },
     {
         title: "Git",
+        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-github"
     },
     {
         title: "Android",
+        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-android"
     }
 ]
