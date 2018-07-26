791369d625cf735998bfe8cb5d452755745a9121
shikdernyc <shikdernyc@gmail.com>
2018-07-26 20:26:06 +0000

    Updated skills into rows and columns

--- src/Skills.js
+++ src/Skills.js
@@ -3,10 +3,30 @@ import skills from './SkillList'
 
 class Skills extends Component{
     render(){
-        let skillList = skills.map(({icon, name}, index)=>{
-            return <i class={icon}>{name}</i>
-        })
-        return skillList
+        let skillList = skills.map(({icon, title, desc}, index)=>(
+          <div key={index} className="card shadow">
+            <i className={icon + " text-center fa-5x mt-2"}/>
+            <div className="card-body">
+              <h5 className="card-title text-center">{title}</h5>
+              <p className="card-text">{desc}</p>
+            </div>
+          </div>
+        ))
+        
+        
+        return(
+            <div id="skills" className="mt-5">
+                <h2 className="text-center mb-4">Skills</h2>
+                <div className="card-deck">
+                    <div className="row mb-3">
+                        {skillList.slice(0,4)}
+                    </div>
+                    <div className="row">
+                        {skillList.slice(4)}
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
