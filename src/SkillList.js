9d1512e14b830a83bb8a78d1f3ad8e827ed4bded
shikdernyc <shikdernyc@gmail.com>
2018-07-25 20:34:24 -0400

    Added a JS object to store skillList. Populated it with current up to date resume data

---
+++ src/SkillList.js
@@ -0,0 +1,36 @@
+const skills = [
+    {
+        title: "Java",
+        icon: "fab fa-java"
+    },
+    {
+        title: "Python",
+        icon: "fab fa-python"
+    },
+    {
+        title: "Javascript",
+        icon: "fab fa-js"
+    },
+    {
+        title: "HTML & CSS",
+        icon: "fab fa-html5"
+    },
+    {
+        title: "NodeJS",
+        icon: "fab fa-node-js"
+    },
+    {
+        title: "React",
+        icon: "fab fa-react"
+    },
+    {
+        title: "Git",
+        icon: "fab fa-github"
+    },
+    {
+        title: "Android",
+        icon: "fab fa-android"
+    }
+]
+
+export default skills
