1f1b2f638a1714f8336f8006c364b17af949b956
shikdernyc <shikdernyc@gmail.com>
2018-07-26 02:31:45 +0000

    Removed Bootstrap import

--- src/index.js
+++ src/index.js
@@ -4,8 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.min.js';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
