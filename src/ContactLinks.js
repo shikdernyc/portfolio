dd89914173e01963a8230f673aa36d4550538bc2
shikdernyc <shikdernyc@gmail.com>
2018-07-27 17:21:14 -0400

    Added a contactlinks js file to store all contact info

---
+++ src/ContactLinks.js
@@ -0,0 +1,24 @@
+const contactLinks = [
+    {
+      icon: "fab fa-github",
+      link: "#"
+    },
+    {
+      icon: "fab fa-instagram",
+      link: "#"
+    },
+    {
+      icon: "fab fa-facebook",
+      link: "#"
+    },
+    {
+      icon: "fab fa-twitter-square",
+      link: "#"
+    },
+    {
+      icon: "fab fa-linkedin",
+      link: "#"
+    }
+]
+
+export default contactLinks
