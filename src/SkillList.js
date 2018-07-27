177fb3cd8faa162d39fb90db3db41cea879b69aa
shikdernyc <shikdernyc@gmail.com>
2018-07-27 11:27:20 -0400

    Refractored skills to be updated with skillPerRow prop. Rendering Skills with internal key

--- src/SkillList.js
+++ src/SkillList.js
@@ -1,40 +1,48 @@
 const skills = [
     {
-        title: "Java",
-        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        icon: "fab fa-java"
-    },
-    {
-        title: "Python",
-        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        icon: "fab fa-python"
-    },
-    {
-        title: "Javascript",
-        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        icon: "fab fa-js"
-    },
-    {
+        key: "skill-0",
         title: "HTML & CSS",
         desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-html5"
     },
     {
-        title: "NodeJS",
+        key: "skill-1",
+        title: "Javascript",
         desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        icon: "fab fa-node-js"
+        icon: "fab fa-js"
     },
     {
+        key: "skill-2",
         title: "React",
         desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-react"
     },
     {
+        key: "skill-3",
+        title: "NodeJS",
+        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        icon: "fab fa-node-js"
+    },
+    {
+        key: "skill-4",
+        title: "Java",
+        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        icon: "fab fa-java"
+    },
+    {
+        key: "skill-5",
+        title: "Python",
+        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        icon: "fab fa-python"
+    },
+    {
+        key: "skill-6",
         title: "Git",
         desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-github"
     },
     {
+        key: "skill-7",
         title: "Android",
         desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
         icon: "fab fa-android"
