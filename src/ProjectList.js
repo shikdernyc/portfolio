a00a4e8593e9eb1dc1f6ee76518ac522415d6129
shikdernyc <shikdernyc@gmail.com>
2018-07-27 15:18:55 -0400

    Updated links and date

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -2,7 +2,7 @@ const projects = [
     {
         key: "proj-0",
         title: "Project Title",
-        img: "https://placeimg.com/640/480/tech/sepia",
+        img: "https://placeimg.com/640/480/tech",
         technology: "React, JS, NPM, Express",
         description: "Labore mollit ad elit dolor. Veniam labore magna consequat aute elit amet sit. Laboris aliqua non occaecat ea. Aute id cillum enim qui sint nulla esse exercitation Lorem in ut. Qui elit labore excepteur ad non et ut nostrud.",
         links: [
@@ -11,21 +11,21 @@ const projects = [
                 link: "#"
             }
         ],
-        date: "current"
+        date: "Current"
     },
     {
         key: "proj-1",
-        img: "https://placeimg.com/640/480/tech/sepia",
+        img: "https://placeimg.com/640/480/tech",
         title: "Project Title",
         technology: "React, JS, NPM, Express",
         description: "Labore mollit ad elit dolor. Veniam labore magna consequat aute elit amet sit. Laboris aliqua non occaecat ea. Aute id cillum enim qui sint nulla esse exercitation Lorem in ut. Qui elit labore excepteur ad non et ut nostrud.",
         links: [
             {
-                label: "Github",
+                label: "Source Code",
                 link: "#"
             },
             {
-                label: "Github",
+                label: "Preview",
                 link: "#"
             }
         ],
@@ -33,7 +33,7 @@ const projects = [
     },
     {
         key: "proj-2",
-        img: "https://placeimg.com/640/480/tech/sepia",
+        img: "https://placeimg.com/640/480/tech",
         title: "Project Title",
         technology: "React, JS, NPM, Express",
         description: "Labore mollit ad elit dolor. Veniam labore magna consequat aute elit amet sit. Laboris aliqua non occaecat ea. Aute id cillum enim qui sint nulla esse exercitation Lorem in ut. Qui elit labore excepteur ad non et ut nostrud.",
@@ -47,7 +47,7 @@ const projects = [
     },
     {
         key: "proj-3",
-        img: "https://placeimg.com/640/480/tech/sepia",
+        img: "https://placeimg.com/640/480/tech",
         title: "Project Title",
         technology: "React, JS, NPM, Express",
         description: "Labore mollit ad elit dolor. Veniam labore magna consequat aute elit amet sit. Laboris aliqua non occaecat ea. Aute id cillum enim qui sint nulla esse exercitation Lorem in ut. Qui elit labore excepteur ad non et ut nostrud.",
