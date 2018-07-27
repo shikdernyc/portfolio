01343593889df049233e18e7c6985028c29ad870
shikdernyc <shikdernyc@gmail.com>
2018-07-27 11:27:56 -0400

    Refractored skills to be updated with skillPerRow prop. Rendering Skills with internal key

--- src/Skills.js
+++ src/Skills.js
@@ -1,29 +1,46 @@
 import React, {Component} from 'react'
 import skills from './SkillList'
 
+const Skill = ({skill}) =>{
+    const {icon, title, desc, key} = skill
+    return (
+        <div key={key} className="card shadow">
+        <i className={icon + " text-center fa-5x mt-2"}/>
+        <div className="card-body">
+          <h5 className="card-title text-center">{title}</h5>
+          <p className="card-text">{desc}</p>
+        </div>
+      </div>
+    )
+}
+
 class Skills extends Component{
+    constructor(props){
+        super(props)
+        this.skillPerRow = props.skillPerRow
+    }
+
     render(){
-        let skillList = skills.map(({icon, title, desc}, index)=>(
-          <div key={index} className="card shadow">
-            <i className={icon + " text-center fa-5x mt-2"}/>
-            <div className="card-body">
-              <h5 className="card-title text-center">{title}</h5>
-              <p className="card-text">{desc}</p>
-            </div>
-          </div>
+        let skillList = skills.map((skill)=>(
+            <Skill skill={skill} />
         ))
+
+        let dynSkillList = [];
+
+        for(let i = 0 ; i< skillList.length ;i+= this.skillPerRow){
+            dynSkillList.push(
+                <div className="row mb-4">
+                    {skillList.slice(i,i+this.skillPerRow)}
+                </div>
+            )
+        }
         
         
         return(
             <div id="skills" className="my-5">
                 <h2 className="text-center mb-4">My Skills</h2>
                 <div className="card-deck">
-                    <div className="row mb-3">
-                        {skillList.slice(0,4)}
-                    </div>
-                    <div className="row">
-                        {skillList.slice(4)}
-                    </div>
+                    {dynSkillList}
                 </div>
             </div>
         )
