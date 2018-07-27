4f061f788c8af141ee62d77f037ff82778adefef
shikdernyc <shikdernyc@gmail.com>
2018-07-27 11:23:36 -0400

    Added project list which will be easily to modify and structure

---
+++ src/ProjectList.js
@@ -0,0 +1,64 @@
+const projects = [
+    {
+        key: "proj-0",
+        title: "Project Title",
+        img: "https://placeimg.com/640/480/tech/sepia",
+        technology: "React, JS, NPM, Express",
+        description: "Labore mollit ad elit dolor. Veniam labore magna consequat aute elit amet sit. Laboris aliqua non occaecat ea. Aute id cillum enim qui sint nulla esse exercitation Lorem in ut. Qui elit labore excepteur ad non et ut nostrud.",
+        links: [
+            {
+                label: "Github",
+                link: "#"
+            }
+        ],
+        date: "current"
+    },
+    {
+        key: "proj-1",
+        img: "https://placeimg.com/640/480/tech/sepia",
+        title: "Project Title",
+        technology: "React, JS, NPM, Express",
+        description: "Labore mollit ad elit dolor. Veniam labore magna consequat aute elit amet sit. Laboris aliqua non occaecat ea. Aute id cillum enim qui sint nulla esse exercitation Lorem in ut. Qui elit labore excepteur ad non et ut nostrud.",
+        links: [
+            {
+                label: "Github",
+                link: "#"
+            },
+            {
+                label: "Github",
+                link: "#"
+            }
+        ],
+        date: "June 2018"
+    },
+    {
+        key: "proj-2",
+        img: "https://placeimg.com/640/480/tech/sepia",
+        title: "Project Title",
+        technology: "React, JS, NPM, Express",
+        description: "Labore mollit ad elit dolor. Veniam labore magna consequat aute elit amet sit. Laboris aliqua non occaecat ea. Aute id cillum enim qui sint nulla esse exercitation Lorem in ut. Qui elit labore excepteur ad non et ut nostrud.",
+        links: [
+            {
+                label: "Github",
+                link: "#"
+            }
+        ],
+        date: "June 2018"
+    },
+    {
+        key: "proj-3",
+        img: "https://placeimg.com/640/480/tech/sepia",
+        title: "Project Title",
+        technology: "React, JS, NPM, Express",
+        description: "Labore mollit ad elit dolor. Veniam labore magna consequat aute elit amet sit. Laboris aliqua non occaecat ea. Aute id cillum enim qui sint nulla esse exercitation Lorem in ut. Qui elit labore excepteur ad non et ut nostrud.",
+        links: [
+            {
+                label: "Github",
+                link: "#"
+            }
+        ],
+        date: "June 2018"
+    },
+]
+
+export default projects
