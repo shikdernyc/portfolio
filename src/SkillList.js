689f17c5a8a53958744c51e888970b81bac58854
shikdernyc <shikdernyc@gmail.com>
2018-07-28 22:24:37 -0400

    Updated skill list desc

--- src/SkillList.js
+++ src/SkillList.js
@@ -2,49 +2,49 @@ const skills = [
     {
         key: "skill-0",
         title: "HTML & CSS",
-        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        desc: "Familiar with HTML5 and CSS3. Have created numerous web projects depending on HTML and CSS. Tend to rely on Bootstrap often",
         icon: "fab fa-html5"
     },
     {
         key: "skill-1",
         title: "Javascript",
-        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        desc: "Very well versed in Vanilla Javascript. Able to work with various data structures and is comfortable with sync and async functions.",
         icon: "fab fa-js"
     },
     {
         key: "skill-2",
         title: "React",
-        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        desc: "Currently learning React and Redux. Working on side projects using React. This current website has been build with React",
         icon: "fab fa-react"
     },
     {
         key: "skill-3",
         title: "NodeJS",
-        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        desc: "Familar with NodeJS on backend. Able to work with express, routers, body parsers, authentication, database and variously commonly used libraries",
         icon: "fab fa-node-js"
     },
     {
         key: "skill-4",
         title: "Java",
-        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        desc: "Very familiar with Java. First language learned and have created varous projects using it. Able to utilize it Android Development",
         icon: "fab fa-java"
     },
     {
         key: "skill-5",
         title: "Python",
-        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        desc: "Very familiar with Python's Data Structure. Able to work with Python's Scientific and Machine Learning Libraries",
         icon: "fab fa-python"
     },
     {
         key: "skill-6",
         title: "Git",
-        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        desc: "Very familar with git version control. Able to properly work in a team and manage small to medium size projects through it",
         icon: "fab fa-github"
     },
     {
         key: "skill-7",
         title: "Android",
-        desc: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        desc: "Novice at Android Development. Have a strong background in Java",
         icon: "fab fa-android"
     }
 ]
