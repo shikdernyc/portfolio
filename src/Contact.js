6c90b4afb5e0ee0c27516b57fd16145ce624e368
shikdernyc <shikdernyc@gmail.com>
2018-07-28 20:53:31 -0400

    Refactored ID's and default texts

--- src/Contact.js
+++ src/Contact.js
@@ -6,24 +6,24 @@ class Contact extends Component{
         return (
             <section id="contact" className="my-5">
                 <div className="container">
-                    <h3 className="text-center mb-4">Let's get in Touch</h3>
-                    <form>
+                    <h3 className="text-center mb-4">Let's get in Touch!</h3>
+                    <form action="mailto:shikdernyc@gmail.com.com" method="post" enctype="text/plain">
                         <div className="form-row">
                             <div className="col mx-3">
                                 <div className="form-group">
-                                    <label htmlFor="exampleInputUsername">Your name</label>
-                                    <input type="text" className="form-control" id placeholder=" Enter Name" />
+                                    <label htmlFor="contact-name">Your name</label>
+                                    <input type="text" className="form-control" id="contact-name" placeholder="Enter Name" />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="exampleInputEmail">Email Address</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail" placeholder=" Enter Email id" />
+                                    <label htmlFor="contact-email">Email Address</label>
+                                    <input type="email" className="form-control" id="contact-email" placeholder="Enter Email Address" />
                                 </div>	
                                 <div className="form-group">
-                                    <label htmlFor="telephone">Mobile No.</label>
-                                    <input type="tel" className="form-control" id="telephone" placeholder=" Enter 10-digit mobile no." />
+                                    <label htmlFor="contact-subject">Subject</label>
+                                    <input type="text" className="form-control" id="contact-subject" placeholder="Enter Email Subject" />
                                 </div>
                             </div>
-                                <div className="col mx-3">
+                                <div className="col">
                                 <div className="form-group">
                                     <label htmlFor="description"> Message</label>
                                     <textarea className="form-control" id="description" rows="6" placeholder="Enter Your Message" defaultValue={""} />
