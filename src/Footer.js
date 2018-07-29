fea090d72fd29d3f94f4c253f713fc1538ae773a
shikdernyc <shikdernyc@gmail.com>
2018-07-28 22:24:10 -0400

    Added keys to social media iterables

--- src/Footer.js
+++ src/Footer.js
@@ -2,12 +2,12 @@ import React, {Component} from 'react'
 import contactLinks from './ContactLinks'
 
 const MediaLink = ({icon, link}) =>(
-    <a href={link}><i className={icon + " fa-lg white-text fa-2x mx-5"}></i></a>
+  <a href={link} rel="noopener noreferrer" target="_blank"><i className={icon + " fa-lg white-text fa-2x mx-5"}></i></a>
 )
 
 class Footer extends Component{
     render(){
-        let socialMedia = contactLinks.map(({icon, link})=>(<MediaLink icon={icon} link={link} />))
+        let socialMedia = contactLinks.map(({icon, link}, index)=>(<MediaLink key={"medialink-"+index} icon={icon} link={link} />))
 
         return (
             <footer className="text-center text-light">
@@ -15,7 +15,7 @@ class Footer extends Component{
                 {socialMedia}
               </div>
               <div className="footer-copyright text-center py-2 bg-dark">© 2018 Copyright:
-                <a href="#"> ShikderNYC.github.io</a>
+                <a href="http://www.shikdertech.io"> shikdertech.io</a>
               </div>
             </footer>
           );
