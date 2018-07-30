91a1daaafd713222af99541de9d9f41325a3b035
shikdernyc <shikdernyc@gmail.com>
2018-07-29 21:34:35 -0400

    Added setAboutContent and defaultContent in the constructor Added a handleTabClick method to handle what will happen once an user clicks a tab componentDidMount sets item 0 on menuItems as active tabs and contents are both being rendered within the About class. Will be refactoring it later

--- src/About.js
+++ src/About.js
@@ -1,48 +1,50 @@
 import React, {Component} from 'react'
 import menuItems from './AboutMeData'
 
-const VTab = ({tab})=>{
-  return (
-    <a className={(tab==="Overview") ? 'nav-link active':"nav-link"} id={"vtab-" + tab + "-tab"} data-toggle="tab" href={"#vtab-"+tab} role="tab" aria-controls={"vtab-"+tab} aria-selected="false">{tab}</a>
-  )
-}
-
-const VContent = ({tab, content}) =>{
-  return(
-    <div className={(tab==="Overview")?"tab-pane fade show active":"tab-pane fade"} id={"vtab-"+tab} role="tabpanel" aria-labelledby={"v-tab-" + tab + "-tab"}>{content}</div>
-  )
-}
-
-const VTabMenu = ({menuItems}) =>{
-  const tabs = menuItems.map((item, index)=>(<VTab key={"vTabMenu-"+index} tab={item.tab}/>))
-  const contents = menuItems.map((item, index)=>(<VContent key={"vContent-"+index} tab={item.tab} content={item.content}/>))
-  return (
-    <div className="row">
-      <div className="col-3">
-        <div className="nav flex-column nav-tabs" id="v-tab" role="tablist" aria-orientation="vertical">
-          {tabs}
-        </div>
-      </div>
-      <div className="col-9 shadow">
-        <div className="tab-content mx-2" id="v-tab-content">
-          {contents}
-        </div>
-      </div>
-    </div>
-  )
-}
-
 class About extends Component{
-  // constructor(props){
-  //     super(props)
-  // }
+  constructor(props){
+      super(props)
+      this.setAboutContent= props.setAboutContent
+      this.defaultContent = menuItems[0].content
+  }
+
+  handleTabClick = function(e){
+    let vTabs = document.getElementsByClassName('v-tab')
+    for(let tab of vTabs){
+      tab.classList.remove('active')
+    }
+    const tab = e.target
+    tab.classList.add('active')
+
+    const newContent = menuItems.find(function(element){return element.tab===tab.innerHTML}).content
+    this.setAboutContent(newContent)
+  }
+
+  componentDidMount(){
+    const vTabs = document.getElementsByClassName('v-tab')
+    vTabs[0].classList.add('active')
+  }
   
   render(){
+    const tabs = menuItems.map(({tab}, index)=>{
+      return <a key={'vtab-'+index} className="nav-link v-tab" href="#about-content" onClick={this.handleTabClick.bind(this)}>{tab}</a>
+    })
+    console.log('Re-Rendering about')
     return (
       <section id="about" className="my-5">
         <div className="container">
-          <h2 className="text-center mb-4">Hi, I'm Asif</h2>
-            <VTabMenu menuItems={menuItems}/>
+          <div className="row">
+            <div className="col-3">
+              <div className="nav flex-column nav-tabs" id="v-tab" role="tablist" aria-orientation="vertical">
+                {tabs}
+              </div>
+            </div>
+            <div className="col-9 shadow">
+              <div className="tab-content mx-2 my-2" id="v-tab-content">
+                {this.props.content==="" ? this.defaultContent : this.props.content}
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     );
